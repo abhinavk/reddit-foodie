6b691c9bcd19095d39bcef11d769d317b9b9be80
Abhinav Kumar <abhinav@outlook.com>
2017-01-05 22:20:57 +0530

    Get and save top 25 posts

--- reddit/foodie.py
+++ reddit/foodie.py
@@ -3,31 +3,52 @@ Foodie
 """
 
 import json
-import string
+import os
 import random
 import requests
 from reddit.post import RedditPost
 from pprint import pprint
 
-
 CLIENT_ID = 'sTUuJHP3q6HjPA'
-
 URL = 'http://www.reddit.com/r/foodporn/.json'
 HEADERS = {'User-Agent': 'abhnv.com\'s reddit-foodie app',}
+
+
+def getSavedLinks():
+    list_files = []
+    if os.path.exists('saves'):
+        list_files = {i.split('.')[0] for i in os.listdir('saves')}
+    return list_files
 
 
 def save(arg='random'):
     pass
 
+
+def downloadLink(post):
+    # pprint(os.listdir('.'))
+    response = requests.get(post.link)
+    if response.status_code == 200:
+        with open('./saves/' + post.name + '.jpg', 'wb') as f:
+            f.write(response.content)
+
+
 def main():
-    res = requests.get(URL, headers=HEADERS)
-    foodDict = json.loads(res.text)
+    response = requests.get(URL, headers=HEADERS)
+    foodDict = json.loads(response.text)
     postsList = foodDict['data']['children'] # It's a list of all posts as dict
     posts = [RedditPost(postDict['data']['name'],
-                        postDict['data']['url']) for postDict in postsList]
+                        postDict['data']['preview']['images'][0]['source']['url'])
+             for postDict in postsList
+             if postDict['data']['stickied'] is False]
+
+    existing_links = getSavedLinks() # Set of names of already saved files
 
     for post in posts:
-        pprint(post.link)
+        if post.name not in existing_links:
+            pprint('Downloading image ' + str(post.serialno))
+            downloadLink(post)
+
 
 if __name__ == '__main__':
     main()
--- reddit/post.py
+++ reddit/post.py
@@ -4,7 +4,14 @@ Reinventing the wheel
 """
 
 class RedditPost(object):
+    """
+    ClassDoc
+    """
+    post_counter = 0
+
     def __init__(self,name,link):
+        self.serialno = RedditPost.post_counter
+        RedditPost.post_counter += 1
         self.link = link
         self.name = name
 
